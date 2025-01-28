aff3382f7f7b666f660a7190397726a9f769227c
Pyae Phyo Aung (Austin) <austin.xu.me@gmail.com>
2025-01-28 12:11:17 +0700

    initial commit

--- AnotherSampleApp/AnotherSampleAppView.swift
+++ AnotherSampleApp/AnotherSampleAppView.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+public class AnotherSampleAppView{
+    public init() { }
+    
+    public static func createView() -> some View{
+        let sampleView = IntroView()
+        return sampleView
+    }
+}
+
+struct IntroView: View {
+    var body: some View {
+        VStack{
+            Text("Welcome from Another Sample App.")
+        }
+    }
+}
